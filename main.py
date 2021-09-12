71ba90ec69da96bc7be7d1cf2782068189965839
Tech With Hasiya <90503467+HasiyaTechlk@users.noreply.github.com>
2021-09-12 17:40:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import os
-import telebot
+from pyrogram import Client, filters
 
-bot = telebot. TeleBot ("1999005975:AAFZVUGOG_z_SLRVJdiuHK3_Abi48Zx-qWI")
+API_ID = 7407598 
+API_HASH = '6e6622a5e352ee0c86cc558ca1c17de3'
+BOT_TOKEN = '1999005975:AAFZVUGOG_z_SLRVJdiuHK3_Abi48Zx-qWI'
 
-@bot.message_handler("commands=["start"])
-def send welcome(message):
-    bot.reply_to(message,"Hello i'm Hasiya Chat Bot")
+bot = Client(
+    'Hasiya-Bot',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
 
-@bot.message_handler(commands=["how"])
-def send_message(message):
-    bot.send_message(message, "https://google.com")
-
-    bot.polling()
+@bot.on_message(filters.command(commands=['start']))
+async def start(client, filters):
+    bot.send_message(chat_id=message.chat.id, text="Hi, How are you? I'm Hasindu's bot.\n Send me some messages.....")
     
+@bot.on_message(filters.command(commands=['help']))
+async def start(client, filters):
+    bot.send_message(chat_id=message.chat.id, text="Just send me some messages and enjoy")
+    bot.run()
