ffa8699cd25dc73eafff03dd3c028a05d7e3c1de
Senith Chandul <79186863+Senith-Chandul@users.noreply.github.com>
2021-09-12 18:06:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,19 @@ from pyrogram import Client, filters
 
 API_ID = 7407598 
 API_HASH = '6e6622a5e352ee0c86cc558ca1c17de3'
-BOT_TOKEN = '1935913054:AAFMHhHsp6PIys3LTWPPa9Re7IRNse2HKNs'
+BOT_TOKEN = '1999005975:AAFZVUGOG_z_SLRVJdiuHK3_Abi48Zx-qWI'
 
-bot = Client(
-    'Hasiya-Bot',
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+bot = Client("Hasiya Bot",
+             api_hash=API_HASH,
+             api_id=API_ID,
+             bot_token=BOT_TOKEN)
 
 @bot.on_message(filters.command(commands=['start']))
-async def start(client, filters):
-    bot.send_message(chat_id=message.chat.id, text="Hi, How are you? I'm Hasindu's bot.\n Send me some messages.....")
+async def welcome(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Hi, How are you? I'm Hasindu's bot.\n Send me some messages.....")
     
 @bot.on_message(filters.command(commands=['help']))
-async def start(client, filters):
-    bot.send_message(chat_id=message.chat.id, text="Just send me some messages and enjoy")
+async def help(client, filters):
+    await bot.send_message(chat_id=message.chat.id, text="Just send me some messages and enjoy")
 
 bot.run()
