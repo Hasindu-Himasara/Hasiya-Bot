fbab865b696acba5889f08a6d92b615bd5df389a
Tech With Hasiya <90503467+HasiyaTechlk@users.noreply.github.com>
2021-09-23 13:19:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ async def help(client, message):
 async def help(client, message):
     await bot.send_message(chat_id=message.chat.id, text="Gaming Hasiya You Tube Channel Link.\n https://www.youtube.com/c/GamingHasiyaYouTube")
     
+    @bot.on_message(filters.command(commands=['Hasindu']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Online 😎")
+    
     
 @bot.on_message(filters.command(commands=['start']))
 async def help(client, message):
