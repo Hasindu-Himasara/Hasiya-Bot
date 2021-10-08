8b1b5d63d1a267a049d4c36925487ca2325cb052
Tech With Hasiya <90503467+HasiyaTechlk@users.noreply.github.com>
2021-10-08 11:49:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ keyboard = InlineKeyboardMarkup(
 
 @bot.on_message(filters.command(commands=['help']))
 async def help(client, message):    
-    await message.reply_photo(photo='https://telegra.ph/file/f3454e8977521dc89dede.jpg',caption='🤗 Hi I am Hasiya LK Bot\n🌐 Tech Wide Group :- https://t.me/TECH_WIDE_GROUP\n🔰 Tech Wide Channel :- https://t.me/TECH_WIDE_OFFICIAL\nStay Safe',reply_markup=keyboard) 
+    await message.reply_photo(photo='https://telegra.ph/file/283b6a1bcefa6af1919f3.jpg',caption='I am Online ✅',reply_markup=keyboard) 
 
 keyboard = InlineKeyboardMarkup( 
              [ 
