6ed2354c6c84d3cbfecd8ea827daf4f7aa117c8b
Tech With Hasiya <90503467+HasiyaTechlk@users.noreply.github.com>
2021-10-08 18:10:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,9 +58,14 @@ keyboard = InlineKeyboardMarkup(
              [ 
                            [InlineKeyboardButton( text="TECH WIDE", url="https://t.me/TECH_WIDE_GROUP")
                            ] 
+            ],
+            [ 
+                           [InlineKeyboardButton( text="TECH WIDE", url="https://t.me/TECH_WIDE_GROUP")
+                           ],
+                           [InlineKeyboardButton( text="TECH WIDE", url="https://t.me/TECH_WIDE_GROUP")
+                           ] 
             ] 
 )
-
 
 
 bot.run() 
